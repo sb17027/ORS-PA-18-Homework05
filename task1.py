f28ad10c46c7c9bc79795131dfc6d89b8c90d15b
Sanela Becovic <sanela.becovic@udg.edu.me>
2019-03-25 12:45:36 +0100

    task1

--- task1.py
+++ task1.py
@@ -15,3 +15,28 @@
 * Use main() function to test your solution.
 ===================================================
 """
+
+import numbers
+
+def numpower(num, expo):
+    i = 1
+    res = 1
+    """ Making generic while loop so we multiply num with itself the precise number of times """
+    """ pom expo helps variable for storing the original value - we need to handle both negative and positive ecpos """
+    pom_expo = abs(expo)
+    while i <= pom_expo:
+        res = res * num
+        i = i + 1
+    """ In cases that we have negative exponent """
+    if expo < 0:
+        res = 1 / res
+
+    return res
+
+def main():
+    num = 2
+    expo = -4
+    res = numpower(num, expo)
+    print("Exponent of a number: ", res)
+
+main()
