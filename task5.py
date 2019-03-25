dcd8325d081e15a414ba2b70bd46c1058f4eae79
Sanela Becovic <sanela.becovic@udg.edu.me>
2019-03-25 12:47:44 +0100

    task5

--- task5.py
+++ task5.py
@@ -13,3 +13,17 @@
 * Use main() function to test your solution.
 ===================================================
 """
+def averageval(numlist):
+    numberOfElements = len(numlist)
+    i = 0
+    sum = 0
+    while i < len(numlist):
+        sum = sum + numlist[i]
+        i = i + 1
+    return sum / numberOfElements
+
+def main():
+    numlist = [1, 2, 3, 4, 5]
+    avg = averageval(numlist)
+    print("Average val is: ", avg)
+main()
